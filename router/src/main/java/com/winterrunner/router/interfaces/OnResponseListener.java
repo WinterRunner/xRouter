20971f481506c36b062ecd57b711b6e97e5b96af
liukunxu <liukunxu@hongyuanshidai.cn>
2017-08-01 10:48:04 +0800

    更新readme

--- router/src/main/java/com/winterrunner/router/interfaces/OnResponseListener.java
+++ router/src/main/java/com/winterrunner/router/interfaces/OnResponseListener.java
@@ -12,7 +12,7 @@ public interface OnResponseListener{
 
     /**
      * @param action 当不需要再接收回调的时候，需要调用aciton.release(...)来释放回调
-     * @param routerResponseBean
+     * @param routerResponseBean 回调bean
      */
     void onSuccess(Action action, RouterResponseBean routerResponseBean);
     void onError();
